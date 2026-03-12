736e5d93ba45ef6a60d77475b8c881425d9b3936
Eray <eraykl0010@gmail.com>
2026-03-13 00:02:38 +0300

    activity_personnel_detail.xml güncellendi. - Mevcut Çalışan bilgisi kutusunun yeri değiştirildi. build.gradle.kts güncellendi. - androidx.core:core-ktx , androidx.core:core olarak güncellendi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     // AndroidX Core
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
